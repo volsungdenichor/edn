1e775da4e77153c44b82b1209b5b4b2ea6d38a25
krzysiek <krzysztof.muhleisen@gmail.com>
2026-02-03 20:01:26 +0100

    parse.test refactored

--- tests/parse.test.cpp
+++ tests/parse.test.cpp
@@ -6,50 +6,32 @@
 
 TEST(parse, integer)
 {
-    EXPECT_THAT(
-        edn::parse("42"), testing::AllOf(testing::Property("if_integer", &edn::value_t::if_integer, testing::Pointee(42))));
+    EXPECT_THAT(edn::parse("42"), IsInteger(42));
 }
 
 TEST(parse, character)
 {
-    EXPECT_THAT(
-        edn::parse(R"(\b)"),
-        testing::AllOf(testing::Property("if_character", &edn::value_t::if_character, testing::Pointee('b'))));
-    EXPECT_THAT(
-        edn::parse(R"(\space)"),
-        testing::AllOf(testing::Property("if_character", &edn::value_t::if_character, testing::Pointee(' '))));
+    EXPECT_THAT(edn::parse(R"(\b)"), IsCharacter('b'));
+    EXPECT_THAT(edn::parse(R"(\space)"), IsCharacter(' '));
 }
 
 TEST(parse, string)
 {
-    EXPECT_THAT(
-        edn::parse(R"("Hello, World!")"),
-        testing::AllOf(
-            testing::Property("if_string", &edn::value_t::if_string, testing::Pointee(testing::StrEq("Hello, World!")))));
+    EXPECT_THAT(edn::parse(R"("Hello, World!")"), IsString(testing::StrEq("Hello, World!")));
 }
 
 TEST(parse, tagged_element_with_space)
 {
     EXPECT_THAT(
         edn::parse(R"(#inst "2024-01-01")"),
-        testing::AllOf(
-            IsTaggedElement(testing::AllOf(
-                TagIs(testing::StrEq("inst")),
-                TaggedElementIs(testing::AllOf(
-                    IsString(testing::StrEq("2024-01-01")),
-                    OfType(edn::value_type_t::string))))),
-            OfType(edn::value_type_t::tagged_element)));
+        IsTaggedElement(
+            testing::AllOf(TagIs(testing::StrEq("inst")), TaggedElementIs(IsString(testing::StrEq("2024-01-01"))))));
 }
 
 TEST(parse, tagged_element_without_space)
 {
     EXPECT_THAT(
         edn::parse(R"(#inst"2024-01-01")"),
-        testing::AllOf(
-            IsTaggedElement(testing::AllOf(
-                TagIs(testing::StrEq("inst")),
-                TaggedElementIs(testing::AllOf(
-                    IsString(testing::StrEq("2024-01-01")),
-                    OfType(edn::value_type_t::string))))),
-            OfType(edn::value_type_t::tagged_element)));
+        IsTaggedElement(
+            testing::AllOf(TagIs(testing::StrEq("inst")), TaggedElementIs(IsString(testing::StrEq("2024-01-01"))))));
 }
